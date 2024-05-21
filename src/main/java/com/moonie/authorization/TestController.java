0ef4774a65ed2190ac650d1de586bd5ec0bd996f
moonie <imooyoni@gmail.com>
2024-05-21 17:19:57 +0900

    swagger tag annotation test

--- src/main/java/com/moonie/authorization/TestController.java
+++ src/main/java/com/moonie/authorization/TestController.java
@@ -1,11 +1,13 @@
 package com.moonie.authorization;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "TEST API", description = "SECURITY 인가 TEST API")
 @RestController
 public class TestController {
     @RequestMapping("/admin")
